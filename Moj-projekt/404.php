b9b3c3f3d7698e35fbfe57842100a94ca6c823be
igoriris <58437979+igoriris@users.noreply.github.com>
2020-10-17 02:45:55 +0200

    Update 404.php

--- Moj-projekt/404.php
+++ Moj-projekt/404.php
@@ -2,4 +2,17 @@
 
 
 
+
+<section class="content-error-404">
+	<div class="container">
+		<div class="row">
+            <div class="center">
+                <h1> 404</h1>
+                <h1> PAGE NOT FOUND</h1>
+                <a href="index" class="btn btn-success center ">BACK TO HOME</a>
+            </div>
+        </div>
+	</div>
+</section>
+
 <?php get_footer()?>
